82431b61cf47e9c50efc382f982e764368a0e3f0
Dmitri <ip.web.so@gmail.com>
2020-05-06 19:12:31 +0300

    feat: add first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from './app.service';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {DatePipe} from '@angular/common';
+import {ImageFormatterComponent} from './image-formatter/image-formatter.component';
+import {LinkFormatterComponent} from './link-formatter/link-formatter.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(
+    private service: AppService,
+    private dateFormatter: DatePipe,
+  ) {
+
+  }
 
   columnDefs = [
-    {headerName: 'Make', field: 'make' },
-    {headerName: 'Model', field: 'model' },
-    {headerName: 'Price', field: 'price'}
+    {headerName: '', cellRendererFramework: ImageFormatterComponent},
+    {headerName: 'Published on', field: 'publishedAt'},
+    {headerName: 'Video Title', cellRendererFramework: LinkFormatterComponent},
+    {headerName: 'Description', field: 'description'}
   ];
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  rowData: Observable<any>;
+
+  ngOnInit(): void {
+    this.rowData = this.service.getData().pipe(
+      map(({items}) => {
+        return items.map(item => {
+          return {
+            ...item.snippet,
+            publishedAt: this.dateFormatter.transform(item.snippet.publishedAt, 'dd.MM.yy hh:mm'),
+            videoId: item.id.videoId
+          };
+        });
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AgGridModule} from 'ag-grid-angular';
+import {HttpClientModule} from '@angular/common/http';
+import {DatePipe} from '@angular/common';
+import {ImageFormatterComponent} from './image-formatter/image-formatter.component';
+import {LinkFormatterComponent} from './link-formatter/link-formatter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageFormatterComponent,
+    LinkFormatterComponent,
   ],
-    imports: [
-        BrowserModule,
-        AgGridModule
-    ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AgGridModule,
+    AgGridModule.withComponents([ImageFormatterComponent]),
+    AgGridModule.withComponents([LinkFormatterComponent])
+  ],
+  providers: [
+    DatePipe,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
