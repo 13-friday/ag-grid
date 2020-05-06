6011113cb4b47a0a9ef413fb389842a5eda08781
Dmitri <ip.web.so@gmail.com>
2020-05-06 17:16:17 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AgGridModule} from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        AgGridModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
